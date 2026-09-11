e12ea8e03c26a0f8f235da1876762a7b5a447b6a
faiz996 <alifaizp10@gmail.com>
2026-09-11 18:03:47 +0530

    Configure Vite for GitHub Pages

--- dental-atelier/vite.config.js
+++ dental-atelier/vite.config.js
@@ -6,12 +6,20 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     allowedHosts: true,
+//   },
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true,
-  },
-})
+  base: "/dental-atelier/",
+});
